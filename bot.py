728bab849eeaa7ef8689d84fa9d0fe1776319724
karazloi321-oss <karazloi321@gmail.com>
2026-06-01 22:54:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-from flask import Flask app = Flask(__name__) @app.route("/") def home(): return """ <h1>Sage Finance работает 🚀</h1> <p>Mini App успешно запущен</p> """ if __name__ == "__main__": app.run(host="0.0.0.0", port=10000)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Sage Finance работает 🚀</h1>
+    <p>Mini App успешно запущен</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
