190735748bca933669c20db66011d9ea15ac389f
karazloi321-oss <karazloi321@gmail.com>
2026-06-05 07:30:53 +0300

    Create db.py

---
+++ core/db.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+DB_NAME = "finance.db"
+
+def get_conn():
+
+    conn = sqlite3.connect(
+        DB_NAME,
+        check_same_thread=False
+    )
+
+    conn.row_factory = sqlite3.Row
+
+    return conn
+
+
+def init_db():
+
+    conn = get_conn()
+
+    c = conn.cursor()
+
+    # USERS
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        telegram_id TEXT UNIQUE,
+        first_name TEXT,
+        username TEXT
+    )
+    """)
+
+    # TRANSACTIONS
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT,
+        type TEXT,
+        amount REAL,
+        category TEXT,
+        timestamp REAL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
